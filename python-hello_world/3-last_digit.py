bfd30377e53d0e91395cc76f90862674f356a714
coder-akash <akashd@brightcomgroup.com>
2022-12-23 10:15:52 +0000

    Task3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000,10000)
+number = random.randint(-10000, 10000)
+no=int(str(number)[-1])
 if number < 0:
-    no = abs(number)
-    if (no%10) == 0:
-        print("Last digit of {} is {} and is 0".format(number, (no%10)))
+    if no == 0:
+        print("Last digit of {} is {} and is 0".format(number, no))
     else:
-        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, (no%10)))
+        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, no))
 else:
-    if (number%10) == 0:
-        print("Last digit of {} is {} and is 0".format(number, (number%10)))
-    elif (number%10) > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, (number%10)))
+    if no == 0:
+        print("Last digit of {} is {} and is 0".format(number, no))
+    elif no > 5:
+        print("Last digit of {} is {} and is greater than 5".format(number, no))
     else:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, (number%10)))
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, no))
+
