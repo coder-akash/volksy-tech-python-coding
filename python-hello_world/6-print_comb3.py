de7603ff69ffc3faa89012513100caf96ba6727a
coder-akash <akashd@brightcomgroup.com>
2022-12-23 16:47:17 +0000

    Task 6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(1, 10):
     print("{:02d}".format(i), end=", ")
-for i in range(10,100):
+for i in range(10, 100):
     if i != int(str(i)[::-1]):
         if i < int(str(i)[::-1]):
             if i == 89:
