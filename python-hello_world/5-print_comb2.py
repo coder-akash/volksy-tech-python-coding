ca0dd26e45847251de4a37aa5351440570162a9f
coder-akash <akashd@brightcomgroup.com>
2022-12-23 12:59:11 +0000

    Task 5

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print(i)
+        print('{}'.format(i))
     else:
-        print ("%02d"%i, end=', ')
-    
+        print ("%02d" %i, end=', ')    
 
